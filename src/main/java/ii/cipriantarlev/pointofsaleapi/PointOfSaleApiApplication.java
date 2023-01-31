6cff94858acf05102f3f3a6c1f3a5ce14013a838
unknown <Ciprian Tarlev>
2023-01-31 17:36:32 +0200

    POS: First Commit

---
+++ src/main/java/ii/cipriantarlev/pointofsaleapi/PointOfSaleApiApplication.java
@@ -0,0 +1,13 @@
+package ii.cipriantarlev.pointofsaleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSaleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ii/cipriantarlev/pointofsaleapi/PointOfSaleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ii.cipriantarlev.pointofsaleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
